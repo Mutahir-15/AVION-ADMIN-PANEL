fe4c0b547035e7a4bb1858506fb1ee6b3ad7b45f
Mutahir Bin Athar <164089040+Mutahir-15@users.noreply.github.com>
2025-02-08 03:31:22 +0500

    Admin Panel

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,33 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 function Header() {
   return (
-    <main className='bg-[#5d737e]'>
-        <section className='max-w-[1440px] mx-auto py-2 grid grid-cols-2 lg:py-4 px-2 lg:px-3'>
-            <div>
-            <h1 className='text-4xl font-bold text-white capitalize text-left'>AVION Admin Dashboard</h1>
-            </div>
-            <div className='flex justify-end items-center'> 
-            <ul className='flex gap-10 text-2xl text-white'>
-                <li>
-                    <Link href='/'>Home</Link>
-                </li>
-                <li>
-                    <Link href='/orders'>Orders</Link>
-                </li>
-                <li>
-                    <Link href='/'>Home</Link>
-                </li>
-            </ul>
-            </div>
-        </section>
-    </main>
-  )
+    <header className="bg-gray-800">
+      <section className="max-w-[1440px] mx-auto py-4 px-6 flex justify-between items-center">
+        <h1 className="text-3xl font-bold text-white">AVION Admin Dashboard</h1>
+        <nav>
+          <ul className="flex gap-8 text-lg text-white">
+            <li>
+              <Link href="/">
+              Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/orders">
+              Orders
+              </Link>
+            </li>
+            <li>
+              <Link href="/dashboard">
+              Dashboard
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
